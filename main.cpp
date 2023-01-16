537a4f9b3b094d844e4ecf560223493bb9610728
Timothy Aluko <taa7@calvin.edu>
2023-01-15 20:52:48 -0500

    Proj 0 done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
+    float arr[20] = {};
 
+    cout << "This is the size of the array: " << sizeof(arr) << endl;
     return 0;
 }
